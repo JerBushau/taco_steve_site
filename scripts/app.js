8c086bd82e7b38580192d87e4aa9caf34d4d0e28
jer bushau <exjerlock@hotmail.com>
2017-08-19 18:06:13 -0400

    Made some changes in color and layout for mobile before starting on desktop

--- scripts/app.js
+++ scripts/app.js
@@ -17,27 +17,47 @@ function initMap() {
 }
 
 // nav menu
+// change this so that the dark-bg is only applied on large devices
+// by checking the width of the window > than 750px
 
 const menuButton = document.getElementById('menu');
 const contactButton = document.getElementById('contact');
 const backButton = document.querySelector('.back-button');
 
+const siteWrapper = document.querySelector('.site-wrapper');
 const menu = document.querySelector('.steve-menu');
-const contact = document.querySelector('.steve-location');
+const contact = document.querySelector('.steve-contact');
 
+// dry up
+siteWrapper.addEventListener('click', e => {
+  let activesSection = document.querySelector('.section-active');
 
-backButton.addEventListener('click', e => {
-  document.querySelector('.active').classList.toggle('active');
-  backButton.classList.toggle('visible');
+  e.preventDefault()
+  if (activesSection && e.target.className === 'site-wrapper dark-bg') {
+    activesSection.classList.remove('section-active');
+    backButton.classList.remove('back-button-visible');
+    siteWrapper.classList.remove('dark-bg');
+  }
+  if (e.target === backButton) backButtonHandler(activesSection);
+  if (e.target === menuButton) menuButtonHandler();
+  if (e.target === contactButton) contactButtonHandler();
 });
 
-menuButton.addEventListener('click', e => {
-  menu.classList.add('active');
-  backButton.classList.add('visible');
-});
+function backButtonHandler(activesSection) {
+  activesSection.classList.toggle('section-active');
+  backButton.classList.toggle('back-button-visible');
+  siteWrapper.classList.remove('dark-bg');
+};
 
-contactButton.addEventListener('click', e => {
-  contact.classList.add('active');
-  backButton.classList.add('visible');
-});
+function menuButtonHandler() {
+  menu.classList.add('section-active');
+  backButton.classList.add('back-button-visible');
+  if (window.innerWidth > 1100) siteWrapper.classList.add('dark-bg');
+};
+
+function contactButtonHandler() {
+  contact.classList.add('section-active');
+  backButton.classList.add('back-button-visible');
+  if (window.innerWidth > 1100) siteWrapper.classList.add('dark-bg');
+};
 
